dffef747657ffd9cf4187a77bae29288c70f991b
BrunoKaike <78989493+BrunoKaike@users.noreply.github.com>
2021-07-05 17:42:24 -0300

    Comentários Doxygen e bug fix

--- src/validation.cpp
+++ src/validation.cpp
@@ -1,27 +1,36 @@
 #include "../include/validation.hpp"
 
+
 vector<string> errorMessage = {"Arquivo não encontrado! tente um novo diretório.",
 "O arquivo não está de acordo com a formatação definida.",
 "Uma ou mais apostas houveram repetição de números.",
-"Uma ou mais apostas possuem um número inválido"};
-vector<string> errors_return;
+"Uma ou mais apostas possuem um número inválido"}; /**< Vetor de string contendo todos os possíveis erros de validação */
 
+vector<string> errors_return; /**< Vetor de string que irá armazenar os erros de validação, caso sejam encontrados */
+
+//! Uma função para identificar se uma string contém apenas números ou pontos.
+/*!
+* \param entrada uma string que representa um fragmento do arquivo de texto contendo o valor apostado ou o número de rodadas.
+* \return um booleano.
+*/
 bool is_number(string entrada){
     return !entrada.empty() && std::find_if(entrada.begin(),
         entrada.end(), [](unsigned char c) { return !std::isdigit(c) && c!='.'; }) == entrada.end();
 }
 
+//! Uma função para identificar se uma string contém uma lista de inteiros separados por espaço.
+/*!
+* \param linha uma string que representa um fragmento do arquivo de texto contendo os números apostados.
+* \return um booleano.
+*/
 bool all_is_numbers(string linha){
-    stringstream ss;
-    size_t start;
-    size_t end = 0;
-    string aux;
+    size_t start; /**< size_t representando uma posição da string, usado para definir a partir de que ponto está o valor a ser validado */
+    size_t end = 0; /**< size_t representando uma posição da string, usado para definir até que ponto está o valor a ser validado */
+    string aux; /**< String representando número a ser validado, caso seja um inteiro */
 
     while((start = linha.find_first_not_of(" ", end)) != std::string::npos){
         end = linha.find(" ", start);
-        ss.clear();
-        ss << linha.substr(start, end - start);
-        ss >> aux;
+        aux = linha.substr(start, end - start);
 
         if(!(!aux.empty() && std::find_if(aux.begin(),
         aux.end(), [](unsigned char c) { return !std::isdigit(c); }) == aux.end())){
@@ -35,11 +44,16 @@ bool all_is_numbers(string linha){
     return true;
 }
 
+//! Uma função para identificar se uma string contém uma lista de inteiros separados por espaço que estão entre o intervalo (1,80).
+/*!
+* \param linha uma string que representa um fragmento do arquivo de texto contendo os números apostados.
+* \return um booleano.
+*/
 bool all_is_in_the_range(string linha){
-    stringstream ss;
-    size_t start;
-    size_t end = 0;
-    int aux;
+    stringstream ss; /**< string stream que será usado para converter o fragmento de string para inteiro */
+    size_t start; /**< size_t representando uma posição da string, usado para definir a partir de que ponto está o valor a ser validado */
+    size_t end = 0; /**< size_t representando uma posição da string, usado para definir até que ponto está o valor a ser validado */
+    int aux; /**< int representando o número a ser validado, caso não seja uma duplicata */
 
     while((start = linha.find_first_not_of(" ", end)) != std::string::npos){
         end = linha.find(" ", start);
@@ -58,11 +72,16 @@ bool all_is_in_the_range(string linha){
     return true;
 }
 
+//! Uma função para identificar se uma string contém uma lista de inteiros separados por espaço que não possuem números repitidos).
+/*!
+* \param linha uma string que representa um fragmento do arquivo de texto contendo os números apostados.
+* \return um booleano.
+*/
 bool repet_num(string linha){
-    stringstream ss;
-    size_t start;
-    size_t end = 0;
-    int aux;
+    stringstream ss; /**< string stream que será usado para converter o fragmento de string para inteiro */
+    size_t start; /**< size_t representando uma posição da string, usado para definir a partir de que ponto está o valor a ser validado */
+    size_t end = 0; /**< size_t representando uma posição da string, usado para definir até que ponto está o valor a ser validado */
+    int aux; /**< int representando o número a ser validado, caso não seja uma duplicata */
     vector<int> numeros;
 
     while((start = linha.find_first_not_of(" ", end)) != std::string::npos){
@@ -80,10 +99,15 @@ bool repet_num(string linha){
     return true;
 }
 
+//! Uma função para identificar se uma string contém uma lista de 1 a 15 inteiros separados por espaço).
+/*!
+* \param linha uma string que representa um fragmento do arquivo de texto contendo os números apostados.
+* \return um booleano.
+*/
 bool qtd_num_aposta(string linha){
-    size_t start;
-    size_t end = 0;
-    int cont=0;
+    size_t start; /**< size_t representando uma posição da string, usado para definir a partir de que ponto está o valor a ser validado */
+    size_t end = 0; /**< size_t representando uma posição da string, usado para definir até que ponto está o valor a ser validado */
+    int cont=0; /**< inteiro representando a quantidade de números separados por espaço presentes na string */
 
     while((start = linha.find_first_not_of(" ", end)) != std::string::npos){
         end = linha.find(" ", start);
@@ -95,8 +119,13 @@ bool qtd_num_aposta(string linha){
     return false;
 }
 
+//! Uma função para verificar se o arquivo existe.
+/*!
+* \param local uma string que representa o diretório do arquivo.
+* \return um booleano.
+*/
 bool arq_inexistente(string local){
-    std::fstream file;
+    std::fstream file; /**< fstream representando um fluxo de entrada/saída para operar em arquivos */
     file.open (local);
     if(!file)
         return false;
@@ -104,6 +133,12 @@ bool arq_inexistente(string local){
     return true;
 }
 
+//! Uma função para validar a formatação do arquivo.
+/*!
+\param file um fstream que representa o arquivo a ser validado.
+\return um booleano.
+\sa is_number(), qtd_num_aposta(), all_is_numbers(), repet_num() and all_is_in_the_range()  
+*/
 bool is_formated(std::fstream& file){
     
     while(!file.eof()){ 
@@ -159,7 +194,12 @@ bool is_formated(std::fstream& file){
 
 }
 
-
+//! Uma função principal para chamar as demais validações.
+/*!
+\param local uma string que representa o diretório do arquivo a ser validado.
+\return um vetor de string.
+\sa arq_inexistente() and is_formated()  
+*/
 vector<string> validate(string local)
 {   
     
