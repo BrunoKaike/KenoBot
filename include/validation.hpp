90f7f47ead35ddd1d26b6d0722fee250fd9913e8
BrunoKaike <78989493+BrunoKaike@users.noreply.github.com>
2021-07-05 18:33:57 -0300

    Comentários

--- include/validation.hpp
+++ include/validation.hpp
@@ -10,20 +10,28 @@
 
 using namespace std;
 
+// Retorna true se uma string contém um número inteiro ou real
 bool is_number(string entrada);
 
+// Retorna true se uma string contém uma lista de números separados por espaço
 bool all_is_numbers(string linha);
 
+// Retorna true se em uma string que contém uma lista de números separados por espaço todos os números estão no intervalo (1,80)
 bool all_is_in_the_range(string linha);
 
+// Retorna true se em uma string que contém uma lista de números separados por espaço não possui números repetidos
 bool repet_num(string linha);
 
+// Retorna true se em uma string que contém uma lista de números separados por espaço possui no mínimo 1 número e no máximo 15
 bool qtd_num_aposta(string linha);
 
+// Retorna true se o arquivo do diretório recebido existe
 bool arq_inexistente(string local);
 
+// Retorna true se o arquivo segue a formatação determinada pelo projeto
 bool is_formated(std::fstream& file);
 
+// Retorna um vetor de strings contendo os erros na validação, caso existam.
 vector<string> validate(string local);
 
 #endif
